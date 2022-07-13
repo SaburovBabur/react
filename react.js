618d29008434e2a7065a4c1c5cf9973d5aef162a
Babur <saburovbabur@gmail.com>
2022-07-13 14:53:44 +0500

    react app init

---
+++ react.js
@@ -0,0 +1,9 @@
+const button = <button>Salom Dunyo</button>
+
+/**
+ * ========================
+ * React app Initialization
+ */
+const app = document.querySelector('#app')
+const root = ReactDOM.createRoot(app)
+root.render(button)
